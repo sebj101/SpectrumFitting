7340ea7f9880b62e1e05fb4ae6efc9f8a3039683
Seb Jones <jonesseb100@gmail.com>
2023-03-06 16:57:50 +0000

    Require C++20 for span

--- src/ToyEndpoint.cpp
+++ src/ToyEndpoint.cpp
@@ -6,8 +6,18 @@
 
 #include "TBetaGenerator.hpp"
 
+#include <span>
+
 int main(int argc, char *argv[])
 {
+  double mBeta{0}; // Effective neutrino mass
+
+  auto args = std::span(argv, size_t(argc));
+  mBeta = std::stod(args[1]);
+
+  // No steriles please
+  const double mN{0};
+  const double eta{0};
 
   return 0;
 }
