330b8594edbbc0991d9100252832f393cf1b9903
Seb Jones <jonesseb100@gmail.com>
2023-03-20 14:07:31 +0000

    Useful numbers

---
+++ src/include/CommonNumbers.h
@@ -0,0 +1,16 @@
+/*
+  CommonNumbers.h
+*/
+
+#ifndef COMMON_NUMBERS_H
+#define COMMON_NUMBERS_H
+
+namespace spec {
+// Seconds in one year
+inline constexpr double ONE_YEAR{31536000};
+
+// Mean tritiuym lifetime in seconds
+inline constexpr double T_MEAN_LIFETIME{560975924};
+}  // namespace spec
+
+#endif
